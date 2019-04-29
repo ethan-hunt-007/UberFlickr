73b102d8601ce096e7cd5a4db0f50ff31411dc88
Jayant Jaiswal <jayantjaiswal@UCLP1234.local>
2019-04-29 11:44:11 +0530

    commited initial projects/frameworks

---
+++ Projects/JJRemoteImage/JJRemoteImage/JJRemoteImage.h
@@ -0,0 +1,19 @@
+//
+//  JJRemoteImage.h
+//  JJRemoteImage
+//
+//  Created by Jayant Jaiswal on 29/04/19.
+//  Copyright © 2019 Jayant Jaiswal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JJRemoteImage.
+FOUNDATION_EXPORT double JJRemoteImageVersionNumber;
+
+//! Project version string for JJRemoteImage.
+FOUNDATION_EXPORT const unsigned char JJRemoteImageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JJRemoteImage/PublicHeader.h>
+
+
--- UberFlickr/AppDelegate.swift
+++ UberFlickr/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import JJRemoteImage
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
